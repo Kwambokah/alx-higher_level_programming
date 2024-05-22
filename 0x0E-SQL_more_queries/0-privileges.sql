fab4c2c8c9375d1637c26f97c7971cb332184ac5
Kwambokah <hellenkwam29@gmail.com>
2024-05-22 23:34:17 +0300

    my privileges

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
--- script that creates the MySQL server user user_0d_1
-CREATE USER IF NOT EXISTS 'user_0d_1' @'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1' @'localhost';
-FLUSH PRIVILEGES;
+--  script that lists all privileges of the MySQL users user_0d_1 and user_0d_2
+
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
